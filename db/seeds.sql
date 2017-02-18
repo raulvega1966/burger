d0414a3c9f118a2d90349b299033536076659a04
raul vega <raul vega>
2017-02-17 19:42:59 -0600

    initial submission

---
+++ db/seeds.sql
@@ -0,0 +1,13 @@
+INSERT INTO burgers (burgername) VALUES ('The Lambshank Redemption');
+INSERT INTO burgers (burgername) VALUES ('The Sweet Sunrise');
+INSERT INTO burgers (burgername) VALUES ('Numb Burger');
+INSERT INTO burgers (burgername) VALUES ('Rings of Fire');
+INSERT INTO burgers (burgername) VALUES ('Baby Got Bok Choy');
+INSERT INTO burgers (burgername) VALUES ('The 38th Parallel');
+
+
+/*
+http://burgerbusiness.com/the-weeks-most-intriguing-burger-names/
+
+
+*/
